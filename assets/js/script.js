437cbaf94d7a9f2a25ae032432b9a6e204396a2e
Nandira Paturohman <nandirasp@gmail.com>
2017-11-21 00:54:33 +0700

    tambah produk dan modal login

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,20 @@
 $('.ui.dropdown')
   .dropdown();
+
+$(document).ready(function(){
+  $('.slider').bxSlider();
+});
+
+$('.bxslider').bxSlider({
+  auto: true,
+  autoControls: true,
+  stopAutoOnClick: true,
+  pager: true,
+  slideWidth: 600
+});
+
+$('#login-modal').click(function() {
+  $('.tiny.modal')
+  .modal('show')
+  ;
+})
